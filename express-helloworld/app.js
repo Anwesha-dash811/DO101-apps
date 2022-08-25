ab48df4298421d5f104dbc65b9943727a0b79e09
Anwesha-dash811 <anweshagcekjr@gmail.com>
2022-08-25 18:21:37 +0530

    new

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Version8 Once again Good Evening !\n '+ new Date());
+  res.send('Good Evening !\n '+ new Date());
 });
 
 app.listen(8080, function () {
